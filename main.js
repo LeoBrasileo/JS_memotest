8eda28116fa9f0336a6f6993538b8acbab0394c2
Leonel Braginski <leobraginski@gmail.com>
2020-08-10 15:54:47 -0300

    fix bugs

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function setBacks () {
     const $carta_atras = document.querySelectorAll(".cara.detras");
     let usados = [];
     //primera vuelta
-    for (let i = 0; i<$carta_atras.length-6; i++) {
+    for (let i = 0; i<$carta_atras.length; i = i + 2) {
         let stringImg = $carta_atras[i].parentNode.getAttribute("id");
         let numImg = Number(stringImg.replace("carta",""));
         let numRN = Math.floor(Math.random() * 6) + 1;
@@ -32,20 +32,14 @@ function setBacks () {
             || numRN === usados[5]) {
             //debo generar un nuevo numero random ente 1 y 6 que no haya salido antes
             let usadofor = 0;
-            for (let i=0; i<usados.length+6; i++) {
+            for (let i=0; i<6000; i++) { //Cuanto mayor es el numero de la condicion menor es el margen de error pero tambien tarda mas en cargar
                 if (numRN === usados[i]
                     || usadofor === usados[0]
                     || usadofor === usados[1]
                     || usadofor === usados[2]
                     || usadofor === usados[3]
                     || usadofor === usados[4]
-                    || usadofor === usados[5]
-                    || usadofor === usados[6]
-                    || usadofor === usados[7]
-                    || usadofor === usados[8]
-                    || usadofor === usados[9]
-                    || usadofor === usados[10]
-                    || usadofor === usados[11]) {
+                    || usadofor === usados[5]) {
                     if (Math.floor(Math.random() * -2) + 1 === 0) {
                         if (numRN === 1) {
                             numRN = Math.floor(Math.random() * (5 - numRN + 1)) + numRN + 1;
@@ -71,7 +65,7 @@ function setBacks () {
     }
     //segunda vuelta
     usados = [];
-    for (let i=6; i<$carta_atras.length; i++) {
+    for (let i=1; i<$carta_atras.length; i = i + 2) {
         let stringImg = $carta_atras[i].parentNode.getAttribute("id");
         let numImg = Number(stringImg.replace("carta",""));
         let numRN = Math.floor(Math.random() * 5) + 1;
@@ -83,20 +77,14 @@ function setBacks () {
             || numRN === usados[4]
             || numRN === usados[5]) {
             let usadofor = 0;
-            for (let i=0; i<usados.length+6; i++) {
+            for (let i=0; i<6000; i++) {
                 if (numRN === usados[i] 
                     || usadofor === usados[0]
                     || usadofor === usados[1]
                     || usadofor === usados[2]
                     || usadofor === usados[3]
                     || usadofor === usados[4]
-                    || usadofor === usados[5]
-                    || usadofor === usados[6]
-                    || usadofor === usados[7]
-                    || usadofor === usados[8]
-                    || usadofor === usados[9]
-                    || usadofor === usados[10]
-                    || usadofor === usados[11]) {
+                    || usadofor === usados[5]) {
                     if (Math.floor(Math.random() * -2) + 1 === 0) {
                         if (numRN === 1) {
                             numRN = Math.floor(Math.random() * (5 - numRN + 1)) + numRN + 1;
